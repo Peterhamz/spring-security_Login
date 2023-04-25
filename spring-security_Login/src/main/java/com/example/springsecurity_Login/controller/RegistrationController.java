dc6c028560ff5dad53625476cd7fd9d41a479ed4
peter <peterhamza6@gmail.com>
2023-04-25 13:47:39 +0100

    adding up the boiler plate code

---
+++ spring-security_Login/src/main/java/com/example/springsecurity_Login/controller/RegistrationController.java
@@ -0,0 +1,7 @@
+package com.example.springsecurity_Login.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+}
---
+++ spring-security_Login/src/main/java/com/example/springsecurity_Login/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.springsecurity_Login.service;
+
+public interface UserService {
+}
